f2511710068b5a71b52989e0cbafaf1e0db25cba
bthank <bthankachan@yahoo.com>
2021-08-23 22:48:26 -0400

    Movied @Profile for I18nEnglishGreetingService to Config class

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -3,8 +3,10 @@ package guru.springframework.sfgdi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.I18nEnglishGreetingService;
 import guru.springframework.sfgdi.services.PrimaryGreetingService;
 import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
 import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
@@ -12,6 +14,13 @@ import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
 @Configuration
 public class GreetingServiceConfig {
 
+	@Profile("EN")
+	@Bean
+	I18nEnglishGreetingService i18nService() {
+		return new I18nEnglishGreetingService();
+	}
+	
+	
 	@Bean
 	@Primary
 	PrimaryGreetingService primaryGreetingService() {
