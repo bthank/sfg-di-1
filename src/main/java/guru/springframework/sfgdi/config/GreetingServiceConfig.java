12026957431c4a5088b6db65e317dc87077c86a7
bthank <bthankachan@yahoo.com>
2021-08-23 22:43:00 -0400

    Modified GreetingServiceConfig class to add primary bean

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -2,14 +2,23 @@ package guru.springframework.sfgdi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
 import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
 import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
 
 @Configuration
 public class GreetingServiceConfig {
 
+	@Bean
+	@Primary
+	PrimaryGreetingService primaryGreetingService() {
+		return new PrimaryGreetingService();
+	};
+	
+	
 	// note that Spring context uses the method name as the Bean name
 	@Bean
 	ConstructorGreetingService constructorGreetingService() {
